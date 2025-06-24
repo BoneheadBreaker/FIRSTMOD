a330cc651e2aa417fdef7f31ce123e5d0af1a8a2
BoneheadBreaker <bhb@tutamail.com>
2025-06-24 18:58:08 +1000

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "FIRSTMOD";
 
-    public const string Description = "My first mod";
+    public const string Description = "This mod gives you monkey knowledge, trophys and cash.";
 
     public const string RepoOwner = "BoneheadBreaker"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "FIRSTMOD"; // TODO add your repo name here, also in the download url in README.md
