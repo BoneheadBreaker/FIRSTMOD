951b5bc493e219ac3ea80dbaf8d6bf6c91724b26
BoneheadBreaker <bhb@tutamail.com>
2025-06-25 18:22:53 +1000

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace Monkey_Money_Tower_XP_and_Trophy_giver;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
     public const string Name = "Monkey Money Tower XP and Trophy giver";
 
-    public const string Description = "This mod gives you monkey knowledge, trophys and cash.";
+    public const string Description = "This mod gives you monkey knowledge, Topwer XP, trophys and cash.";
 
     public const string RepoOwner = "BoneheadBreaker"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "FIRSTMOD"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "Monkey_Money_Tower_XP_and_Trophy_giver"; // TODO add your repo name here, also in the download url in README.md
 }
